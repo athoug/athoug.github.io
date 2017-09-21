ee33e9eba0214aeacf3372c537308cdc4fd6b3ac
Athoug Alsoughayer <athoug2.s@gmail.com>
2017-09-21 20:24:18 +0300

    add php files

---
+++ index.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="./vendors/css/normalize.css">
+    <link rel="stylesheet" href="./vendors/css/grid.css">
+    <link rel="stylesheet" href="./vendors/css/ionicons.min.css">
+    <link rel="stylesheet" href="./vendors/css/animate.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="./css/master.min.css">
+    <link rel="stylesheet" href="./css/queries.min.css">
+
+    <title>Athoug Alsoughayer</title>
+</head>
+
+<body>
+    <header class="head" id="about">
+        <nav>
+            <div class="row">
+                <ul class="main-nav js-main-nav">
+                    <li><a href="#about">about</a></li>
+                    <li><a href="#projects">projects</a></li>
+                    <li><a href="#writing">writing</a></li>
+                    <li><a href="#contact">contact</a></li>
+                </ul>
+                <a class="mobile-nav-icon js-mobile-nav"><i class="ion-navicon"></i></a>
+            </div>
+        </nav>
+
+        <div class="row face">
+          <img src="./media/img/athoug-white.gif" alt="athoug face">
+        </div>
+
+        <div class="row about-container">
+            <div class="about-content name">
+                <a href="#" id="name-audio"><h1>Athoug Alsoughayer &nbsp;<span class="audio"><i class="ion-ios-volume-high"></i></span></h1></a>
+                <p>forever in a state of wonder</p>
+            </div>
+            <div class="about-content about">
+                <h1>About</h1>
+                <p class="about-text">I am a developer with a strong interest in art, design, and animation. Usually, I spend my time programming and teaching how to code. Whenever I find some free time, I like to experiment with creative coding and game development. For a
+                    much formal about, <a href="./media/cv.pdf" id="officla-docs" download>click here</a>.</p>
+            </div>
+            <div class="links">
+                <ul class="bottom-links">
+                    <li><a href="https://github.com/athoug" target="_blank"><i class="ion-social-github"></i></a></li>
+                    <li><a href="https://codepen.io/athoug/" target="_blank"><i class="ion-social-codepen"></i></a></li>
+                    <li><a href="#" target="_blank"><i class="ion-social-youtube"></i></a></li>
+                    <li><a href="https://twitter.com/to0ogi" target="_blank"><i class="ion-social-twitter"></i></a></li>
+                    <li><a href="https://athougcodesatnight.tumblr.com" target="_blank"><i class="ion-social-tumblr"></i></a></li>
+                    <li><a href="#" target="_blank"><i class="ion-social-wordpress"></i></a></li>
+                </ul>
+            </div>
+        </div>
+
+        <audio id="athoug-sound" src="./media/athoug.wav"></audio>
+    </header>
+
+    <section class="projects" id="projects">
+        <div class="row project-title">
+            <h1>Projects</h1>
+        </div>
+        <div class="row js-wp-1" id="portfolio">
+            <div class="tile">
+                <a href="https://athoug.xyz/" target="_blank" data-linktext="180 day project" class="slide_from_left"><img src="https://media.giphy.com/media/3o7aCWBFqbJLP6LGLK/giphy.gif" alt="180 day project" /></a>
+            </div>
+
+            <div class="tile">
+                <a href="https://athoug.xyz/day30/index.html" target="_blank" data-linktext="Sputnik" class="slide_from_left"><img src="https://media.giphy.com/media/xT9IgyFJrxgu6JXzrO/giphy.gif" alt="Sputnik" /></a>
+            </div>
+
+            <div class="tile">
+                <a href="https://athoug.xyz/day52/index.html" target="_blank" data-linktext="Timer" class="slide_from_left"><img src="https://media.giphy.com/media/xT9IgIxlF8SIpPRdTi/giphy.gif" alt="Timer" /></a>
+            </div>
+
+            <div class="tile">
+                <a href="https://athoug.xyz/day53/index.html" target="_blank" data-linktext="Portrait" class="slide_from_left"><img src="https://media.giphy.com/media/3ohhwk71YWf6sScbug/giphy.gif" alt="Portrait" /></a>
+            </div>
+
+            <div class="tile">
+                <a href="https://athoug.xyz/day57/index.html" target="_blank" data-linktext="Space Walk" class="slide_from_left"><img src="https://media.giphy.com/media/xT9IgHPRaqtnX9RcgE/giphy.gif" alt="Space Walk" /></a>
+            </div>
+
+            <div class="tile">
+                <a href="https://athoug.xyz/day12/index.html" target="_blank" data-linktext="Digital Clock" class="slide_from_left"><img src="https://media.giphy.com/media/l1J9zFSvGP2OSQmd2/giphy.gif" alt="Digital Clock" /></a>
+            </div>
+
+            <div class="tile">
+                <a href="https://athoug.xyz/day55/index.html" target="_blank" data-linktext="Weather App" class="slide_from_left"><img src="https://media.giphy.com/media/xT9IgH5TbyGsmqCRXO/giphy.gif" alt="Weather App" /></a>
+            </div>
+
+            <div class="tile">
+                <a href="https://athoug.xyz/day16/index.html" target="_blank" data-linktext="Calculator" class="slide_from_left"><img src="https://media.giphy.com/media/xT9IgvR2FpkaidQGAM/giphy.gif" alt="Calculator" /></a>
+            </div>
+
+            <div class="tile">
+                <a href="https://athoug.xyz/day34/index.html" target="_blank" data-linktext="Arduino Simulator" class="slide_from_left"><img src="https://media.giphy.com/media/3ov9jFBZ3fQKQnjkVa/giphy.gif" alt="Arduino Simulator" /></a>
+            </div>
+
+            <div class="tile">
+                <a href="https://athoug.xyz/day36/index.html" target="_blank" data-linktext="Quote Machine" class="slide_from_left"><img src="https://media.giphy.com/media/3o7aCXT9n8vCrnTxcY/giphy.gif" alt="Quote Machine" /></a>
+            </div>
+
+            <div class="tile">
+                <a href="https://athoug.xyz/day51/index.html" target="_blank" data-linktext="To-Do List" class="slide_from_left"><img src="https://media.giphy.com/media/3o7aD0MNyQAr45uWek/giphy.gif" alt="To-Do List" /></a>
+            </div>
+
+            <div class="tile">
+                <a href="https://athoug.xyz/day50/index.html" target="_blank" data-linktext="Code Player" class="slide_from_left"><img src="https://media.giphy.com/media/l1J9Ej7wkCQy66fZe/giphy.gif" alt="Code Player" /></a>
+            </div>
+
+            <div class="tile">
+                <a href="https://athoug.xyz/day43/index.html" target="_blank" data-linktext="Converter" class="slide_from_left"><img src="https://media.giphy.com/media/l378rKv8PEDyugmw8/giphy.gif" alt="Converter" /></a>
+            </div>
+
+            <div class="tile">
+                <a href="https://athoug.xyz/day40/index.html" target="_blank" data-linktext="Creative Coding" class="slide_from_left"><img src="https://media.giphy.com/media/3o7aDeSyrmx9DtyNzy/giphy.gif" alt="Creative Coding" /></a>
+            </div>
+
+            <div class="tile">
+                <a href="https://athoug.xyz/day25/index.html" target="_blank" data-linktext="Happy/Sad" class="slide_from_left"><img src="https://media.giphy.com/media/3o7aDbrjC8oaCanZ5K/giphy.gif" alt="Happy/Sad" /></a>
+            </div>
+
+            <div class="tile">
+                <a href="https://athoug.xyz/day11/index.html" target="_blank" data-linktext="Lost Bit" class="slide_from_left"><img src="https://media.giphy.com/media/l378BlnDNyfEq2F0c/giphy.gif" alt="Lost Bit" /></a>
+            </div>
+
+            <div class="tile">
+                <a href="https://athoug.xyz/day13/index.html" target="_blank" data-linktext="Tesla Vs Edison" class="slide_from_left"><img src="https://media.giphy.com/media/xT9IgE352mtRjbtpLi/giphy.gif" alt="Tesla Vs Edison" /></a>
+            </div>
+
+            <div class="tile">
+                <a href="https://athoug.xyz/day39/index.html" target="_blank" data-linktext="Trumpland" class="slide_from_left"><img src="https://media.giphy.com/media/3ohhwBUeeFsTV4aIMw/giphy.gif" alt="Trumpland" /></a>
+            </div>
+
+        </div>
+    </section>
+
+    <section class="contact" id="contact">
+        <div class="row contact-title">
+            <h1>Contact</h1>
+        </div>
+
+
+        <div class="row form" id="form">
+            <form class="contact-form" action="mailer.php" method="post">
+
+                <?php
+                    if($_GET['success'] == 1) {
+                        echo "<div class=\"form-messages success\">
+                                Thank you! Your message has been sent
+                            </div>";
+                    }
+
+                    if($_GET['success'] == -1) {
+                        echo "<div class=\"form-messages error\">
+                                Oops! Something went wrong. Please try again.
+                            </div>";
+                        }
+                ?>
+
+                <div class="row">
+                    <div class="col span-3-of-3">
+                        <input type="text" name="name" id="name" placeholder="NAME" required>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col span-3-of-3">
+                        <input type="email" name="email" id="email" placeholder="EMAIL">
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col span-3-of-3">
+                        <textarea name="message" placeholder="MESSAGE" rows="14" id="mesage"></textarea>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col span-3-of-3">
+                        <input type="submit" id="send" value="Send it!">
+                    </div>
+                </div>
+
+            </form>
+        </div>
+    </section>
+
+    <footer>
+        <div class="row footer">
+            <div class="col span-1-of-2">
+                <ul class="footer-nav">
+                    <li><a href="#about">about</a></li>
+                    <li><a href="#projects">projects</a></li>
+                    <li><a href="#writing">writing</a></li>
+                    <li><a href="#contact">contact</a></li>
+                </ul>
+            </div>
+            <div class="col span-1-of-2">
+                <ul class="social-links">
+                    <li><a href="https://github.com/athoug" target="_blank"><i class="ion-social-github"></i></a></li>
+                    <li><a href="https://codepen.io/athoug/" target="_blank"><i class="ion-social-codepen"></i></a></li>
+                    <li><a href="#" target="_blank"><i class="ion-social-youtube"></i></a></li>
+                    <li><a href="https://twitter.com/to0ogi" target="_blank"><i class="ion-social-twitter"></i></a></li>
+                    <li><a href="https://athougcodesatnight.tumblr.com" target="_blank"><i class="ion-social-tumblr"></i></a></li>
+                    <li><a href="#" target="_blank"><i class="ion-social-wordpress"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/g/respond@1.4.2,html5shiv@3.7.3"></script>
+    <script src="https://cdn.jsdelivr.net/selectivizr/1.0.3b/selectivizr.min.js"></script>
+    <script src="./vendors/js/jquery.waypoints.min.js"></script>
+    <script src="./js/script.js"></script>
+</body>
+
+</html>
